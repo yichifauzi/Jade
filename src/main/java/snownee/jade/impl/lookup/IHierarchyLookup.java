a1dde4d8a2ec9653a42f239fc59cb193fb9354df
Snownee <i@snownee.com>
2024-12-22 02:35:16 +0800

    fix: error while remapping data provider ids caused by different instances with the same id

--- src/main/java/snownee/jade/impl/lookup/IHierarchyLookup.java
+++ src/main/java/snownee/jade/impl/lookup/IHierarchyLookup.java
@@ -4,16 +4,17 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.jetbrains.annotations.Nullable;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Streams;
 
+import net.minecraft.core.IdMap;
 import net.minecraft.core.IdMapper;
 import net.minecraft.resources.ResourceLocation;
+import snownee.jade.Jade;
 import snownee.jade.api.IJadeProvider;
 import snownee.jade.impl.PriorityStore;
 
@@ -61,7 +62,7 @@ public interface IHierarchyLookup<T extends IJadeProvider> {
 			idMapper = new IdMapper<>(list.size());
 		}
 		for (T provider : list) {
-			if (idMapper.getId(provider) == -1) {
+			if (idMapper.getId(provider) == IdMap.DEFAULT) {
 				idMapper.add(provider);
 			}
 		}
@@ -70,7 +71,17 @@ public interface IHierarchyLookup<T extends IJadeProvider> {
 
 	default void remapIds(List<ResourceLocation> ids) {
 		IdMapper<T> idMapper = Objects.requireNonNull(idMapper());
-		Map<ResourceLocation, T> map = Streams.stream(idMapper).collect(Collectors.toMap(IJadeProvider::getUid, Function.identity()));
+		Map<ResourceLocation, T> map = Maps.newHashMapWithExpectedSize(idMapper.size());
+		Streams.stream(idMapper).forEach(provider -> {
+			T oldProvider = map.put(provider.getUid(), provider);
+			if (oldProvider != provider && oldProvider != null) {
+				Jade.LOGGER.warn(
+						"Found different data providers with same id {}, this may cause issues: {} and {}",
+						provider.getUid(),
+						oldProvider,
+						provider);
+			}
+		});
 		int i = 0;
 		for (ResourceLocation id : ids) {
 			T object = map.get(id);
